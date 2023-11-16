c5117bb7257898191db3560ca81eb595e995f52f
Guilherme Lage da costa <guilhermeldcosta@gmail.com>
2023-11-16 20:44:53 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,11 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         /*
-         1 - Parametros para gerar de grafos:
+         1) Parametros para gerar de grafos:
           - Grafos eulerianos, semi-eulerianos ou nao-eulerianos
           - Numero de vertices como 100, 1.000, 10.000 ou 100.000
 
-         2 - Para testar os grafos, usar a classe GraphGenerator. Exemplos com 100.000 vertices:
+         2) Para testar os grafos, usar a classe GraphGenerator. Exemplos com 100.000 vertices:
           - GraphGenerator.eulerian(100000)
           - GraphGenerator.semiEulerian(100000)
           - GraphGenerator.nonEulerian(100000)
